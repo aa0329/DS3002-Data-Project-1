3cc66eedf981ff26a33315093e6c695e9dd09ba0
aa0329 <65728258+aa0329@users.noreply.github.com>
2021-10-24 18:35:39 -0400

    Complete first operation for data processor

---
+++ ds3002proj1.py
@@ -0,0 +1,13 @@
+# Operation 1: Fetch / download / retrieve a remote data file by ingesting a local file
+
+# load cvs module 
+import csv
+
+# open file for reading 
+url = 'world-happiness-report-2019.csv'
+with open(url) as csvDataFile:
+# read file as csv file 
+    csvReader = csv.reader(csvDataFile)
+# for every row, print the row
+    for row in csvReader:
+        print(row)
