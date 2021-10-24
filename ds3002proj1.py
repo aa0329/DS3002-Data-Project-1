5e03d620545a08ac5b18fba8896a64fe4319cda6
aa0329 <65728258+aa0329@users.noreply.github.com>
2021-10-24 18:36:33 -0400

    Remove print statements from file

--- ds3002proj1.py
+++ ds3002proj1.py
@@ -8,6 +8,3 @@ url = 'world-happiness-report-2019.csv'
 with open(url) as csvDataFile:
 # read file as csv file 
     csvReader = csv.reader(csvDataFile)
-# for every row, print the row
-    for row in csvReader:
-        print(row)
