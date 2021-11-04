98b6a541f0afb895b309706d6c7091dc3a11c7d0
aa0329 <65728258+aa0329@users.noreply.github.com>
2021-11-04 14:43:46 -0400

    Add comments

--- ds3002proj1.py
+++ ds3002proj1.py
@@ -34,10 +34,12 @@ with open(url) as original:
         data.append(row)
         rowCount = rowCount + 1
     # Convert python list to JSON 
+
+# Operation 4: The converted (new) file should be written to disk
 with open(jsonFile, 'w', encoding='utf-8') as json_file:
     json_file.write(json.dumps(data, indent=5))
 
-# Operation 3: Generate a brief summary of the data file ingestion
+# Operation 5: Generate a brief summary of the data file ingestion
 print("This is a brief summary of the data file ingestion:")
 print("The number of rows in this file is: " + str(rowCount))
 print("The number of columns in this file is: " + str(ncol))
